1cbe10de881334494066ee2d86acfea3f8ef4f2d
Mohit <mohitkg2601@gmail.com>
2023-11-22 11:34:31 +0530

    about us page update

--- src/Components/Header/About/About.jsx
+++ src/Components/Header/About/About.jsx
@@ -10,15 +10,13 @@ const About = () => {
     <div className="about">
       <div className="aboutus">
         <h1>About us</h1>
-        <p>At Bag Store, our mission is to redefine the way you experience bags. We believe that a bag is not just an accessory;
-           it's an extension of your personality and a statement of your style. Our commitment is to craft and curate a collection 
-            of bags that seamlessly blend fashion, function, and durability.</p>
-          <p>We are passionate about bags in all their forms, from sleek and sophisticated to casual and carefree.
-           Our mission is to provide you with a diverse range of high-quality bags that cater to your unique needs
-            and preferences, whether you're a trendsetter, a practical traveler, a professional on-the-go, or an adventure 
-            enthusiast. Sustainability and ethics are at the core of our values. We are dedicated to
-          sourcing eco-friendly materials and ensuring ethical practices throughout our supply chain.
-           We believe in making responsible choices that not only enhance your bag experience but also contribute to a better world.</p>
+        <p>At ShoesStore, we believe that the right pair of shoes can elevate your style and enhance your comfort.
+           we have been dedicated to providing our customers with a curated selection of footwear that combines fashion and functionality.</p>
+          <p>We are passionate about quality craftsmanship and only offer shoes from reputable brands known for their commitment to excellence.
+             Each pair in our collection is carefully chosen to ensure it meets our high standards for durability, style, and comfort.
+             At ShoesStore, we value our community and the environment. We actively seek to contribute positively to both by supporting local initiatives and offering sustainable footwear options.
+              Our commitment to ethical business practices reflects our dedication to making a positive impact beyond the world of shoes.
+              </p><p>Thank you for choosing ShoesStore as your go-to destination for quality footwear. We look forward to being part of your journey, one step at a time.</p>
       </div>
       <div className="button">
         <button>Shop Now</button>
